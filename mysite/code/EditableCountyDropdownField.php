bd2c623a7084e1c2e69c6aa431bdcec783258488
Dustin Quam <quamsta@gmail.com>
2016-02-22 17:16:27 -0600

    county dropdown

---
+++ mysite/code/EditableCountyDropdownField.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * A dropdown field which allows the user to select a country
+ *
+ * @package userforms
+ */
+class EditableCountyDropdownField extends EditableFormField {
+
+	private static $singular_name = 'Iowa County Dropdown';
+
+	private static $plural_name = 'Iowa County Dropdowns';
+
+	/**
+	 * @return FieldList
+	 */
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+
+		$fields->removeByName('Default');
+
+		return $fields;
+	}
+
+	public function getFormField() {
+		$field =  DropdownField::create($this->Name, $this->EscapedTitle, County::get()->map('Title'))
+			->setEmptyString('(Select a county)')
+			->setFieldHolderTemplate('UserFormsField_holder')
+			->setTemplate('UserFormsDropdownField');
+
+		$this->doUpdateFormField($field);
+
+		return $field;
+	}
+
+	public function getValueFromData($data) {
+		if(isset($data[$this->Name])) {
+			$source = $this->getFormField()->getSource();
+			return $source[$data[$this->Name]];
+		}
+	}
+
+	public function getIcon() {
+		return USERFORMS_DIR . '/images/editabledropdown.png';
+	}
+
+	public function getSelectorField(EditableCustomRule $rule, $forOnLoad = false) {
+		return "$(\"select[name='{$this->Name}']\")";
+	}
+}
